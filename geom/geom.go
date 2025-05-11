965d4bbf28e94d6bb753d81964e5f545f10dabb4
Dan Tull <dantull@gmail.com>
2025-05-11 11:19:16 -0500

    Generalized Stringify to use for Board display.

--- geom/geom.go
+++ geom/geom.go
@@ -116,9 +116,37 @@ func Stringify(ps []Point, blank rune, fill rune) []string {
 	bounds := Bounds(ps)
 	width := bounds[1].X - bounds[0].X + 1
 	height := bounds[1].Y - bounds[0].Y + 1
+
+	pmap := make(map[int]map[int]bool)
+
+	for _, p := range ps {
+		if _, ok := pmap[p.X]; !ok {
+			pmap[p.X] = make(map[int]bool)
+		}
+
+		pmap[p.X][p.Y] = true
+	}
+
+	var convert = func(p Point) string {
+		offset := AddPoints(p, bounds[0])
+		if xmap, ok := pmap[offset.X]; !ok {
+			if _, ok := xmap[offset.Y]; ok {
+				return string(fill)
+			} else {
+				return string(blank)
+			}
+		}
+
+		return string(blank)
+	}
+
 	strs := make([]string, height)
 	for i := range strs {
-		strs[i] = strings.Repeat(string(blank), width)
+		sb := strings.Builder{}
+		for j := 0; j < width; j++ {
+			sb.WriteString(convert(Point{X: j, Y: i}))
+		}
+		strs[i] = sb.String()
 	}
 
 	for _, p := range ps {
