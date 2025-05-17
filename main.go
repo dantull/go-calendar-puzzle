838a37dc606056d311544179d464c1a918208b21
Dan Tull <dantull@gmail.com>
2025-05-17 11:26:06 -0500

    Fix a couple of bugs and add verbose mode
    The minSize value was still set to 3, but should be 4 now.
    The V shape had a misplaced point.
    
    Added a verbose mode to dump all the shapes and try placing them.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"calendar-puzzle/geom"
 	"calendar-puzzle/solver"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -16,7 +17,6 @@ func main() {
 		geom.Point{X: 6, Y: 7}, geom.Point{X: 5, Y: 7}, geom.Point{X: 4, Y: 7})
 
 	b := board.NewBoard(ps)
-	board.FillPoints(b, []geom.Point{{X: 4, Y: 0}, {X: 3, Y: 3}, {X: 3, Y: 6}}, "X")
 
 	var boardAt = func(p geom.Point) string {
 		if label := board.LabelAt(b, p); label != nil {
@@ -36,8 +36,6 @@ func main() {
 		fmt.Println()
 	}
 
-	dumpBoard()
-
 	labeledShapes := map[string]geom.Shape{
 		"I": *geom.NewShape(false, 1, []geom.Point{
 			{X: 0, Y: 0}, {X: 1, Y: 0}, {X: 2, Y: 0}, {X: 3, Y: 0}}),
@@ -56,22 +54,52 @@ func main() {
 		"P": *geom.NewShape(true, 3, []geom.Point{
 			{X: 0, Y: 0}, {X: 1, Y: 0}, {X: 2, Y: 0}, {X: 0, Y: 1}, {X: 1, Y: 1}}),
 		"V": *geom.NewShape(false, 3, []geom.Point{
-			{X: 0, Y: 0}, {X: 1, Y: 0}, {X: 2, Y: 0}, {X: 0, Y: 1}, {X: 1, Y: 2}}),
+			{X: 0, Y: 0}, {X: 1, Y: 0}, {X: 2, Y: 0}, {X: 0, Y: 1}, {X: 0, Y: 2}}),
 		"Z": *geom.NewShape(true, 1, []geom.Point{
 			{X: 0, Y: 0}, {X: 1, Y: 0}, {X: 1, Y: 1}, {X: 1, Y: 2}, {X: 2, Y: 2}}),
 	}
 
-	solverStepper := solver.CreateSolver(b, labeledShapes, 3)
+	verbose := len(os.Args) >= 2 && os.Args[1] == "-v"
+
+	// try to place each shape in position 0, 0
+	for label, shape := range labeledShapes {
+		vs := geom.Variants(&shape)
+		placed := false
+
+		for _, v := range vs {
+			remove := board.FillPoints(b, v, label)
+
+			if remove != nil {
+				placed = true
+				if verbose {
+					dumpBoard()
+				}
+				(*remove)()
+				break
+			}
+		}
+		if !placed {
+			fmt.Printf("Failed to place %s shape\n", label)
+			return
+		}
+	}
+
+	board.FillPoints(b, []geom.Point{{X: 4, Y: 0}, {X: 3, Y: 3}, {X: 3, Y: 6}}, "X")
+	dumpBoard()
+
+	solverStepper := solver.CreateSolver(b, labeledShapes, 4)
 
 	done := false
 
 	for {
 		more := solverStepper(func(inspector solver.Inspector, event solver.Event) {
-
 			if event.Kind == "solved" {
 				dumpBoard()
 				fmt.Println("Solved!")
 				done = true
+			} else if verbose {
+				dumpBoard()
+				fmt.Printf("Event: %s\n", event.Kind)
 			}
 		})
 
